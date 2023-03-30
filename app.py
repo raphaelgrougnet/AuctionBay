b7f0a11e77df640254d3d80176a6d0f83075441f
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-03-30 14:32:46 -0400

    Ajout Fichiers de base

---
+++ app.py
@@ -0,0 +1,23 @@
+"""
+Démonstration des paramètres obligatoires
+"""
+
+from flask import Flask, redirect, render_template, request, abort, session
+import hashlib
+
+app = Flask(__name__)
+
+import bd
+
+app = Flask(__name__)
+
+# Enregistre toutes les routes disponibles dans dp_jeu avec le préfixe /jeu
+
+
+app.secret_key = "fbfd893893844ef4da62c134e0d61a47117d1bc3a33cb78d0a144576d23d2bf3"
+
+
+@app.route('/')
+def index():
+    """Affiche l'accueil"""
+    return render_template('index.jinja')
---
+++ bd.py
@@ -0,0 +1,43 @@
+"""
+Connexion à la BD
+"""
+
+import types
+import contextlib
+import mysql.connector
+import hashlib
+
+
+@contextlib.contextmanager
+def creer_connexion():
+    """Pour créer une connexion à la BD"""
+    conn = mysql.connector.connect(
+        user="garneau",
+        password="qwerty123",
+        host="127.0.0.1",
+        database="binette_book",
+        raise_on_warnings=True
+    )
+
+    # Pour ajouter la méthode get_curseur() à l'objet connexion
+    conn.get_curseur = types.MethodType(get_curseur, conn)
+
+    try:
+        yield conn
+    except Exception:
+        conn.rollback()
+        raise
+    else:
+        conn.commit()
+    finally:
+        conn.close()
+
+
+@contextlib.contextmanager
+def get_curseur(self):
+    """Permet d'avoir les enregistrements sous forme de dictionnaires"""
+    curseur = self.cursor(dictionary=True)
+    try:
+        yield curseur
+    finally:
+        curseur.close()
